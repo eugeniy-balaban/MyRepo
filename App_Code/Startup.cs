f95b77751673a264cac2ce0659782c1255cc65f0
eugeniy.balaban <eugeniy.balaban@devexpress.com>
2016-01-14 11:23:28 +0300

    create new project

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyRepo.Startup))]
+namespace MyRepo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
